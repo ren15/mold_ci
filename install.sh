bb1cf05dde63ec664de1a25a749ba602f9acb6c2
Ren Wang <iamwrm@gmail.com>
2022-01-29 16:52:22 +0000

    use filename

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-wget -c https://nightly.link/ren15/mold_ci/workflows/build/main/mold_compile_art.zip
-unzip mold_compile_art.zip
+FILE_NAME=mold_compile_art.zip
+wget -c https://nightly.link/ren15/mold_ci/workflows/build/main/${FILE_NAME}
+unzip ${FILE_NAME}
+rm ${FILE_NAME}
 
 chmod +x mold
 mkdir -p /usr/local/lib/mold
