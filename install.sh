0b173d7c9b798215783589705bd73c865a3de29d
Ren Wang <iamwrm@gmail.com>
2022-01-29 16:17:52 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+wget -c https://nightly.link/ren15/mold_ci/workflows/build/main/mold_complie_art.zip 
+unzip mold_compile_art.zip
+
+chmod +x mold
+mkdir -p /usr/local/lib/mold
+
+mv mold /usr/local/bin
+mv mold-wrapper.so /usr/local/lib/mold
