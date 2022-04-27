9417e2f75250a4dbf5c6094c3970ad4590bd2935
iamwrm <iamwrm@gmail.com>
2022-04-27 14:14:17 +0800

    dont remove

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ tar xvf mold.tar.gz
 chmod +x mold
 mkdir -p /usr/local/lib/mold
 
-rm mold.tar.gz
-mv mold /usr/local/bin
-mv mold-wrapper.so /usr/local/lib/mold
+#rm mold.tar.gz
+cp mold /usr/local/bin
+cp mold-wrapper.so /usr/local/lib/mold
